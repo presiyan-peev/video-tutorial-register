a2f029d7e0498339064b4a1192a2e1ed5889847a
Presiyan Peev <pressijan@gmail.com>
2020-06-01 23:03:49 +0300

    create pages sceleton

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/public/Home.vue'
+import Login from '../views/public/Login.vue'
+import Register from '../views/public/Register.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,22 @@ Vue.use(VueRouter)
     component: Home
   },
   {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'Register',
+    component: Register
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/public/About.vue')
   }
 ]
 
