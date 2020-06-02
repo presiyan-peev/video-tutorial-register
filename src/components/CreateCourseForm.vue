f2d1608451da64a83123a22aad0ccb3a1e68582b
Presiyan Peev <pressijan@gmail.com>
2020-06-03 00:25:01 +0300

    add create course view

--- src/components/CreateCourseForm.vue
+++ src/components/CreateCourseForm.vue
@@ -1,20 +1,94 @@
 <template>
   <div class="createCourseForm">
-    xyu
-    
+    <Title>
+      Create a Course
+    </Title>
+    <v-row>
+        <v-col cols="0" sm="3" md="3">
+        </v-col>
+        <v-col cols="12" sm="6" md="6">
+            <v-card class="px-4">
+                <v-card-text>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field v-model="courseTitle" :rules="[rules.required]" label="Course Title" maxlength="20" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-textarea v-model="coursDesc" :rules="[rules.required]" label="Course Description" maxlength="50" required></v-textarea>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field v-model="courseImg" :rules="[rules.required]" label="Image url" maxlength="100" required></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col class="d-flex mr-auto" cols="6" sm="3" xsm="3">
+                                <v-checkbox
+                                  v-model="isPublic"
+                                  label="public"
+                                ></v-checkbox>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col class="d-flex ml-auto" cols="6" sm="3" xsm="3">
+                                <v-btn x-large block depressed :disabled="!valid" color="primary lighten-1" @click="validate">Submit</v-btn>
+                            </v-col>
+                        </v-row>
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col cols="0" sm="3" md="3">
+        </v-col>
+    </v-row>  
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Title from '@/components/helpers/Title.vue'
+
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'CreateCourseForm',
+  components: {
+    Title
+  },
   computed: {
     ...mapGetters([
         'isAdmin'
     ])
-  }
+  },
+
+  methods: {
+    validate() {
+      if (this.$refs.loginForm.validate()) {
+        // submit form to server/API here...
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    }
+  },
+  data: () => ({
+    valid: true,
+    
+    courseTitle: "",
+    courseDesc: "",
+    courseImg: "",
+    isPublic: false,
+    lectures: [],
+    usersEnrolled: [],
+    lecturesCount: 0,
+    usersEnrolledCount: 0,
+    verify: "",
+    show1: false,
+    rules: {
+      required: value => !!value || "Required.",
+      min: v => (v && v.length >= 8) || "Min 8 characters"
+    }
+  })
 }
 </script>
